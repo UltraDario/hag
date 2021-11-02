621dc241240c129977af326502ea43d7a9391f75
Ultra_Dario <dariobienzobas32@gmail.com>
2021-11-02 18:52:03 +0100

    mierda

---
+++ hag2/hag2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hag2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hola");
+        }
+    }
+}
